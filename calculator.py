f9e23547676ff271d0742c43b825cb3513b16aa0
TessyJr <christopher.nathanael1217@gmail.com>
2022-03-06 18:32:24 +0700

    updated python file

--- calculator.py
+++ calculator.py
@@ -10,24 +10,52 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
-operation = 0;
+def repeat():
+    again = input("Do another calculation? (y/n): ")
+    if again == 'y':
+        selectOperation();
+    elif again == 'n':
+        print("Thank you for using my calculator")
+    else:
+        repeat();
 
-while operation != '1' and operation != '2' and operation != '3' and operation != '4':
+def calculate(num1, num2, operation):
+    if operation == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif operation == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif operation == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+    elif operation == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+    repeat();
+    
+
+def inputNumber(operation):
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    calculate(num1, num2, operation);
+
+def selectOperation():
     print("Select operation.")
     print("1.Add")
     print("2.Subtract")
     print("3.Multiply")
     print("4.Divide")
     operation = input("Enter operation (1/2/3/4): ")
-    
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
 
-if operation == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
-elif operation == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
-elif operation == '3':
-        print(num1, "*", num2, "=", multiply(num1, num2))
-elif operation == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
+    if operation != '1' and operation != '2' and operation != '3' and operation != '4':
+        selectOperation();
+    else:
+        inputNumber(operation);
+    
+
+selectOperation();
+
+
+
+
+
+    
+
+
